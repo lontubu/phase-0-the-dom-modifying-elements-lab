a6f0e17ac82adea286042873d20c7c9227f3b436
lontubu <nickson.lontubu@studentmoringa.com>
2023-10-03 09:56:16 +0300

    assighment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+ const main = document.getElementById('main');
+// main.innerHTML();
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id",'victory');
+newHeader.innerHTML ="NICKSON is the champion";
+
